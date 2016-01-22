46b4741b3ce504a685c8e6b3a2fd6e055e143b57
caramellaio <albertobombardelli123@gmail.com>
2016-01-22 11:51:00 +0100

    added neig methods (doesn't run)

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var neig_gesture;
+var neig_gesture,rgn;
 function void_region(){return {tank:1,player:null,path:null}}
 function region(_n_tanks,_player,_path){
     var self = this;
@@ -44,14 +44,14 @@ function region(_n_tanks,_player,_path){
     }
     
     this.path.onmouseover=function(){
-        console.log("move_over");
+        //console.log("move_over");
         self.path.classList.remove(self.selected?"pressed":"land");
         self.path.classList.add("mouseover");
         neig_gesture.mouse_over = self;
     }
     
     this.path.onmouseout=function(){
-        console.log("move out");
+        //console.log("move out");
         self.path.classList.remove("mouseover");
         self.path.classList.add(self.selected?"pressed":"land");
         neig_gesture.mouse_out = self;
@@ -61,7 +61,7 @@ function region(_n_tanks,_player,_path){
 
 function a(){
     neig_gesture = new neig_gest();
-    var rgn=[];
+    rgn = [];
     var paths = document.getElementById("it").contentDocument.children[0].getElementsByTagName("g")[0].getElementsByTagName("path");
     for(var i=0;i<paths.length;i++)
         rgn.push(new region(3,null,paths[i]));
--- neig_gest.js
+++ neig_gest.js
@@ -6,7 +6,7 @@ var neig_gest= function(){
     this._mouse_over= undefined;
     this._mouse_out=undefined;
     this.__defineSetter__("mouse_over",function(val){
-        console.log("setting mouse_over value to"+val);
+        //console.log("setting mouse_over value to"+val);
         this._mouse_over=val;
         if(self._mouse_over!== undefined && self._mouse_out !== undefined){
             self.add_neig();
@@ -15,7 +15,7 @@ var neig_gest= function(){
     );
     
     this.__defineSetter__("mouse_out",function(val){
-        console.log("setting mouse_out value to "+val);
+        //console.log("setting mouse_out value to "+val);
         this._mouse_over = undefined;
         this._mouse_out=val;
         });
@@ -26,7 +26,45 @@ var neig_gest= function(){
             el1.neig = [];
         if(!contains(el1.neig,self._mouse_over))
             el1.neig.push(self.mouse_over);
-        console.log("added neig to: "+el1.path);
+        console.log([el1.path,self._mouse_over]);
+    }
+
+    this.calculateM = function (rgn) {
+        var lst = self.get_all_region_point_objs(rgn);
+        for (var i = 0; i < lst.length; i++) {
+            for(var k=i+1;k<lst.length;k++)
+                if (lst[i].intersect(lst[k])&& !list[i].nei.contains(lst[k])) {
+                    lst[i].nei.add(lst[k]);
+                    lst[k].nei.add(lst[i]);
+                }
+        }
+        console.log(lst); 
+    }
+
+    this.get_all_region_point_objs=function(rgn){
+        var lst = [];
+        for (var i = 0; i < rgn.length; i++) {
+            var intrsct = function(num) {
+                return lst.contains(num)
+            }
+            lst.push({ nums: (rgn[i].path.getAttribute("d").split(',')), region: rgn[i] });
+            var numss = lst[i].nums;
+            var intrsct = function (nums) {
+                for (var i = 0; i < nums.length; i++) {
+                    if (numss.contains(nums[i]))
+                        return true;
+                }
+                return false;
+            }
+            lst[i].intersect = intrsct;
+            lst[i].nei = [];
+            console.log(lst[i]);
+            for (var k = 0; k < lst[i].nums.length; k++)
+                lst[i].nums[k] = parseFloat(lst[i].nums[k].substring(1));
+            console.log(lst[i]);
+        }
+        console.log(lst);
+        return lst;
     }
 }
 
