b51acb40a68f5ca66a39608f00fff98410c27dc6
caramellaio <albertobombardelli123@gmail.com>
2016-01-25 16:09:28 +0100

    temp_commit

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var neig_gesture,rgn;
+var neig_gesture,rgn,player_count;
 function void_region(){return {tank:1,player:null,path:null}}
 function region(_n_tanks,_player,_path){
     var self = this;
@@ -6,7 +6,8 @@ function region(_n_tanks,_player,_path){
     this.player=_player;
     this.nei = [];
     this.path=_path;
-    this.selected=false;
+    this.selected = false;
+
     this.atk=function(el){
         var a =[], b=[];
         for(var i=0;i<self.n_tanks;i++)
@@ -58,6 +59,26 @@ function region(_n_tanks,_player,_path){
     //end click zone
 }
 
+function player(_regions, _total_army, _color) {
+    
+    var self = this;
+    this.id = player_count;
+    this.regions = _regions;
+    this._army_count = _total_army;
+    this.__defineGetter__("army_count", function () { var count = 0; for (var i = 0; i < self.regions.length; i++) { count += self.regions[i].n_tanks; } });
+    var create_style = function (color) {
+        var style = document.createElement("style");
+        style.type = "text/css";
+        // continue here
+        style.innerHTML = ".player" + self.id + " {fill-opacity: 1; stroke:white;stroke-opacity: 1;stroke-width:0.5;}";
+    }
+    this.atk = function (str_rgn, dest_rgn) {
+        if (self.regions.includes(str_rgn)&& str_rgn.nei.includes(dest_rgn) && str_rgn.n_tanks>=2) {
+            str_rgn.atk(dest_rgn);
+        } else{alert("cannot attack!!!")}
+    }
+    player_count++;
+}
 function a(){
     neig_gesture = new neig_gest();
     rgn = [];
